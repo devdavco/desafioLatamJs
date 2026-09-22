b005bb00add1cc4b40e1f3165b38b8785b0c6eec
devdavco <github.ferocity308@passmail.com>
2026-09-22 15:55:17 -0500

    array pos

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+
+Crea una función llamada ultimoYPrimero que reciba un arreglo y retorne la concantenación del último y el primer elemento del arreglo.
+
+*/ 
+
+
+function ultimoYPrimero(arreglo){
+    return (arreglo[arreglo.length-1]+arreglo[0])
+}
